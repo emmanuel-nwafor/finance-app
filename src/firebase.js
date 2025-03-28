d8f76fe8d0f593418ef2f64c332fa544be5532ee
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-03-28 01:36:51 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAULQVv40WQCq_Z7CHR8JoMSHlCouRKbTw",
+  authDomain: "finance-banking-app.firebaseapp.com",
+  projectId: "finance-banking-app",
+  storageBucket: "finance-banking-app.firebasestorage.app",
+  messagingSenderId: "631788965438",
+  appId: "1:631788965438:web:8cd52a927a3ca55e3ecb6f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
