cec249313909842b526300791f98d16de5416fcd
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-03-28 01:49:52 -0700

    all commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ function App() {
 
   return (
     <>
-      <h1></h1>
+      <div className="">
+
+            <h1 className="text-3xl font-bold underline">
+              Hello world!
+            </h1>
+
+      </div>
     </>
   )
 }
